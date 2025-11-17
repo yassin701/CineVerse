9ffe3fcede0d03191fd8adce7f3bf8c15869ce30
Yassine <yassinehamdoune55@gmail.com>
2025-11-18 00:49:17 +0100

    creat contact and styling it

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Contact() {
   return (
-    <div>
+    <div className="contact-container">
       <h2>Contactez-nous</h2>
       <form>
         <div>
           <label>Name :</label>
-          <input type="text" name="name"/>
-          <label>Email :</label>
-          <input type="email" name="email"/>
-          <label>Message :</label>
-          <textarea name="message"/>
+          <input type="text" name="name" />
         </div>
         <div>
-            <button type="submit">Send</button>
+          <label>Email :</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>Message :</label>
+          <textarea name="message" rows="5" />
+        </div>
+        <div>
+          <button type="submit">Send</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
