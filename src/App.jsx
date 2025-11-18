2a775f14aa59c18d57ac40fc55f4745bb61a048d
Yassine <yassinehamdoune55@gmail.com>
2025-11-18 16:46:21 +0100

    Create clickable movie images and movie detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
-import Home from './pages/Home'
-import Contact from './Components/Contact'
-import './App.css'
-import { BrowserRouter,Routes , Route } from 'react-router-dom'
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Contact from "./Components/Contact";
+import "./App.css";
 
 function App() {
-
   return (
-
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Contact' element={<Contact/>}/>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/details/:id" element={<MovieDetails />} />
+       
       </Routes>
-    
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import hero from '../image/image_Home.jpeg'
-import slide1 from '../image/slide1.png'
-import slide2 from '../image/slide2.png'
-import slide3 from '../image/slide3.png'
-import slide4 from '../image/slide4.png'
-
-const slides = [slide1, slide2, slide3, slide4, slide4, slide4, slide4, slide4, slide4, slide4];
+import React from "react";
+import { Link } from "react-router-dom";
+import movies from "../data/movies";
+import hero from "../image/image_Home.jpeg";
 
 export default function Home() {
   return (
     <>
-      {/* HERO SECTION */}
+      {/* IMAGE HERO */}
       <div className="hero-banner">
-        <img src={hero} alt="Hero Banner" className="hero-img" />
+        <img src={hero} className="hero-img" alt="Hero" />
       </div>
 
-      {/* MOVIES ROW */}
+      {/* LISTE DES FILMS */}
       <div className="movies-row">
-        {slides.map((slide, index) => (
-          <img key={index} src={slide} alt={`slide${index + 1}`} className="movie-card" />
+        {movies.map((movie) => (
+          <Link key={movie.id} to={`/details/${movie.id}`}>
+            <img src={movie.image} alt={movie.title} className="movie-card" />
+          </Link>
         ))}
       </div>
     </>
-  )
+  );
 }
